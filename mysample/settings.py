5f84ea80746f108dda9d10789b3031221fdc1af7
tkamalakar <64142997+tkamalakar@users.noreply.github.com>
2023-02-15 15:10:12 +0530

    Update settings.py

--- mysample/settings.py
+++ mysample/settings.py
@@ -15,6 +15,10 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+print(os.environ.get('APP_ENV'))
+print(os.environ.get('APP_ENV', 'Kamal'))
+print(os.getenv('APP_ENV', 'DefaultKamal'))
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
